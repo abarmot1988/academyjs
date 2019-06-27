414d673645cce78ca193b37f1ff0af6d822f1bb3
abarmot <z.voronkov@mail.ru>
2019-06-28 00:33:26 +0300

    первый коммит

---
+++ main.js
@@ -0,0 +1,11 @@
+let money = 100;
+let income = 200;
+let addExpenses = 300;
+let deposit = 400;
+let mission = 500;
+let period = 600;
+
+alert ("все готово)))");
+
+console.log (period);
+// console.log('не работает jshint)))');
